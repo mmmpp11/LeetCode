1758fd1e9548c3785766569383e5c5d25a3e4edf
mmp <1285344622@qq.com>
2021-06-29 22:06:26 +0800

    168.Excel表列名称

---
+++ src/ExerciseJava168.java
@@ -0,0 +1,25 @@
+public class ExerciseJava168 {
+
+
+    private StringBuilder str = new StringBuilder();
+
+    public String convertToTitle(int columnNumber) {
+        dfs(columnNumber);
+        str.reverse();
+        return str.toString();
+
+    }
+
+    private void dfs(int num){
+        if (num == 0){
+            return;
+        }
+        num--;
+        str.append((char) ('A'+num % 26));
+        dfs(num / 26);
+    }
+
+    public static void main(String[] args) {
+        System.out.println((char) ('A'+701% 26));
+    }
+}
---
+++ src/ExerciseKotlin168.kt
@@ -0,0 +1,30 @@
+import java.lang.StringBuilder
+
+
+/**
+ * 168. Excel表列名称
+ */
+class ExerciseKotlin168 {
+
+    private val str = StringBuilder()
+
+    fun convertToTitle(columnNumber: Int): String {
+        dfs(columnNumber)
+        return str.reverse().toString()
+
+    }
+
+    private fun dfs(num: Int){
+        if (num == 0){
+            return
+        }
+        val temp = num-1
+        str.append('A' + temp % 26)
+        dfs(temp / 26)
+    }
+
+}
+
+fun main() {
+    println(ExerciseKotlin168().convertToTitle(701))
+}
