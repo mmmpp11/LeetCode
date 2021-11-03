4f546ef7e1a3ee3d65c8945cbde0a9d95bff382e
mmp <1285344622@qq.com>
2021-11-03 20:30:23 +0800

    242. 有效的字母异位词

---
+++ src/string/code242/Java242.java
@@ -0,0 +1,27 @@
+package string.code242;
+
+import java.util.Arrays;
+
+/**
+ * 242. 有效的字母异位词
+ * 给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的字母异位词。
+ *
+ * 注意：若 s 和 t 中每个字符出现的次数都相同，则称 s 和 t 互为字母异位词
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/valid-anagram
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Java242 {
+    public boolean isAnagram(String s, String t) {
+
+        if (s.equals(t)){
+            return false;
+        }
+        char[] a = s.toCharArray();
+        char[] b = t.toCharArray();
+        Arrays.sort(a);
+        Arrays.sort(b);
+        return Arrays.toString(a).equals(Arrays.toString(b));
+    }
+}
