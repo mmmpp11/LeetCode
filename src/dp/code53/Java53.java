489a5aeb17b2180ebda945ebcd2237906d633fd5
mmp <1285344622@qq.com>
2021-11-30 22:24:50 +0800

    121. 买卖股票的最佳时机

---
+++ src/dp/code53/Java53.java
@@ -0,0 +1,21 @@
+package dp.code53;
+
+/**
+ * 53. 最大子数组和
+ */
+
+public class Java53 {
+    public int maxSubArray(int[] nums) {
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        int max = dp[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            dp[i] = Math.max(nums[i], dp[i-1]+ nums[i]);
+            max = Math.max(max,dp[i]);
+        }
+
+        return max;
+    }
+
+}
